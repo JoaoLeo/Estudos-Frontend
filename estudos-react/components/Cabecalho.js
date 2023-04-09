391c9bc2a3412892ec6ebaaa9e5a0b69c26d74d5
João Leonardo <joaoleonardo9921@gmail.com>
2023-04-09 14:25:39 -0300

    Adicionando page de carros

--- estudos-react/components/Cabecalho.js
+++ estudos-react/components/Cabecalho.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {Navbar, Container, Nav} from 'react-bootstrap'
 
 const Cabecalho = () => {
@@ -6,11 +6,12 @@ const Cabecalho = () => {
   <> 
     <Navbar bg="dark" variant="dark">
     <Container>
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Navbar.Brand href="#">Sistema Acadêmico</Navbar.Brand>
       <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
+        <Nav.Link href="/">Página 1</Nav.Link>
+        <Nav.Link href="/pagina2">Página 2</Nav.Link>
+        <Nav.Link href="/array">Array</Nav.Link>
+        <Nav.Link href="/carros">Carros</Nav.Link>
       </Nav>
     </Container>
   </Navbar>

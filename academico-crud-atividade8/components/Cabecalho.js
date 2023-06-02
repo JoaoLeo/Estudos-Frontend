f74b659f0f16ef302d7784f47edf9f66088977d2
João <joaoleonardo9921@gmail.com>
2023-06-02 09:59:23 -0300

    alterando cabeçalho

--- academico-crud-atividade8/components/Cabecalho.js
+++ academico-crud-atividade8/components/Cabecalho.js
@@ -9,9 +9,8 @@ const Cabecalho = () => {
           <Nav className="me-auto">
             <Nav.Link href="/cursos">Cursos</Nav.Link>
             <Nav.Link href="/disciplinas">Disciplinas</Nav.Link>
-            <Nav.Link href="/alunos">Alunos</Nav.Link>
             <Nav.Link href="/professores">Professores</Nav.Link>
-            <Nav.Link href="/turmas">Turmas</Nav.Link>
+            <Nav.Link href="/alunos">Alunos</Nav.Link>          
             <Nav.Link href="/salas">Salas</Nav.Link>
             <Nav.Link href="/semestres">Semestres</Nav.Link>
           
